474dbf6a395c14781d89700f1edb7869b3c41aae
piyush <piyush.sinha24@gmail.com>
2019-01-18 12:14:34 +0530

    alert dialog added

--- lib/pages/product_page.dart
+++ lib/pages/product_page.dart
@@ -6,6 +6,32 @@ class ProductPage extends StatelessWidget {
   final String imgurl;
   ProductPage(this.title, this.imgurl);
 
+  _showWarning(BuildContext context) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Are you sure?'),
+            content: Text('This action cannot be undone!'),
+            actions: <Widget>[
+              FlatButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text('DISCARD'),
+              ),
+              FlatButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                  Navigator.pop(context, true);
+                },
+                child: Text('CONTINUE'),
+              ),
+            ],
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
@@ -26,7 +52,7 @@ class ProductPage extends StatelessWidget {
                   padding: EdgeInsets.all(10.0), child: Image.asset(imgurl)),
               Container(padding: EdgeInsets.all(10.0), child: Text(title)),
               RaisedButton(
-                onPressed: () => Navigator.pop(context, true),
+                onPressed: () => _showWarning(context),
                 child: Text('Delete'),
                 color: Theme.of(context).primaryColor,
               )
