b05bd63226f7ec00f4a38b59dd4cf50b6cc374d1
piyush <piyush.sinha24@gmail.com>
2019-01-20 17:12:31 +0530

    Login page

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -2,7 +2,31 @@ import 'package:flutter/material.dart';
 
 import './products.dart';
 
-class AuthPage extends StatelessWidget {
+class AuthPage extends StatefulWidget {
+  @override
+  _AuthPageState createState() => new _AuthPageState();
+}
+
+class _AuthPageState extends State<AuthPage> with SingleTickerProviderStateMixin {
+
+  AnimationController _iconAnimationController;
+  Animation<double> _iconAnimation;
+
+  @override
+  void initState()
+  {
+    super.initState();
+    _iconAnimationController=new AnimationController(
+        vsync:this,
+        duration: new Duration(milliseconds: 500)
+    );
+    _iconAnimation=new CurvedAnimation(
+        parent: _iconAnimationController,
+        curve: Curves.easeOut,
+    );
+    _iconAnimation.addListener(()=>this.setState((){}));
+    _iconAnimationController.forward();
+  }
   String loginid = '';
   String password = '';
   @override
@@ -11,36 +35,62 @@ class AuthPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Login'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              TextField(
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(labelText: 'Login ID'),
-                onChanged: (String value) {
-                  loginid = value;
-                },
-              ),
-              TextField(
-                decoration: InputDecoration(labelText: 'Password'),
-                onChanged: (String value) {
-                  password = value;
-                },
-              ),
-              RaisedButton(
-                child: Text('LOGIN'),
-                textColor: Colors.white,
-                color: Colors.red,
-                onPressed: () {
-                  Navigator.pushReplacementNamed(context, '/p');
-                },
-              ),
-            ],
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          new Image(
+            image: new AssetImage("assets/background.jpg"),
+            fit: BoxFit.cover,
           ),
-        ),
+          Container(
+            margin: EdgeInsets.all(20.0),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  new FlutterLogo(
+                    size: _iconAnimation.value*100,
+                    colors: Colors.red,
+                  ),
+                  TextField(
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      hintText: 'Username',
+                      icon: Icon(Icons.assignment_ind),
+                    ),
+                    onChanged: (String value) {
+                      loginid = value;
+                    },
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Password',
+                      icon: Icon(Icons.lock),
+                    ),
+                    obscureText: true,
+                    onChanged: (String value) {
+                      password = value;
+                    },
+                  ),
+                  SizedBox(
+                    height: 50.0,
+                  ),
+                  RaisedButton(
+                    child: Text('LOGIN'),
+                    textColor: Colors.white,
+                    color: Colors.red,
+                    onPressed: () {
+                      Navigator.pushReplacementNamed(context, '/p');
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
